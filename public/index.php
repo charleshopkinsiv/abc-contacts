b232decace50837c74cda5ca827d134fe11103a9
Charles Hopkins <charleshopkinsiv@gmail.com>
2022-10-25 22:29:49 +0000

    Minor edit

--- public/index.php
+++ public/index.php
@@ -2,12 +2,6 @@
 
 use Abc\controller\ContactController;
 
-ini_set('memory_limit','16000000M');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require __DIR__ . "/../vendor/autoload.php";
-
 $controller = new ContactController();
 $controller->index();
